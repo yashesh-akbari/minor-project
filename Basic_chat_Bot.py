e67509db59e29f59f2c0b246cde69c14b36323f8
Yashesh Akbari <159016243+yashesh-akbari@users.noreply.github.com>
2024-08-06 09:14:57 +0530

    Basic_chat_Bot.py

--- Basic_chat_Bot.py
+++ Basic_chat_Bot.py
@@ -7,5 +7,4 @@ age_difference=age-bot_age
 print(f"You are {age_difference} years older than me. "
 +f"I'm only {bot_age} Years old!")
 color_input=input("What's your Favorite color? ")
-color=color_input
-print(f"Oh, {color} is a beautiful color!")
+print(f"Oh, {color_input} is a beautiful color!")
